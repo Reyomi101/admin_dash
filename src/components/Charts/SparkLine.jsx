9f9a5777b9c6d9bf067e592edf9f3746db0ff463
Reyomi101 <78329356+Reyomi101@users.noreply.github.com>
2022-10-14 15:37:07 +0800

    Order and Employees page done

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
+import { SparklineComponent } from '@syncfusion/ej2-react-charts'
 
-const SparkLine = () => {
-  return <div>SparkLine</div>
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="yval"
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        format: '${x} : data ${yval}',
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  )
 }
 
 export default SparkLine
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar,
+} from '@syncfusion/ej2-react-grids'
+import { employeesData, employeesGrid } from '../data/dummy'
+import { Header } from '../components'
 
 const Employees = () => {
-  return <div>Employees</div>
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Employees" category="Page" />
+      <GridComponent
+        toolbar={['Search']}
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  )
 }
 
 export default Employees
