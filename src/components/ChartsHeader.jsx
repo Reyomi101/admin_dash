d2fd7f1cf03c56106e5b199fd534836766423105
Reyomi101 <78329356+Reyomi101@users.noreply.github.com>
2022-10-14 11:00:31 +0800

    init or first commit

---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ChartsHeader = () => {
+  return <div>ChartHead</div>
+}
+
+export default ChartsHeader
---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const UserProfile = () => {
+  return <div>UserPorfile</div>
+}
+
+export default UserProfile
